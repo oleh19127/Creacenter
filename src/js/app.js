6d5e19faff1a9364bada91c21bc7db55dd6907ec
oleh312 <40184919+oleh312@users.noreply.github.com>
2022-12-12 10:20:59 +0200

    remove unused images and js

--- src/js/app.js
+++ src/js/app.js
@@ -30,16 +30,11 @@ LAYZR(VANILLA JS) https://github.com/callmecavs/layzr.js
 
 */
 
-import { checkDevice } from "./modules/checkDevice.mjs";
-import { isWebp } from "./modules/isWebp.mjs";
-import { ibg } from "./modules/ibg.mjs";
-import { spoilersActive } from "./modules/spoilers.mjs";
-
 // VARIABLES
-const menuLinksData = document.querySelectorAll(".menu__link[data-goto]");
+// const menuLinksData = document.querySelectorAll(".menu__link[data-goto]");
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
-const menuLinks = document.querySelectorAll(".menu__link");
+// const menuLinks = document.querySelectorAll(".menu__link");
 const menuArrows = document.querySelectorAll(".menu__arrow");
 const load = document.querySelector(".loading");
 const sublists = document.querySelectorAll(".menu__sublist");
@@ -49,35 +44,53 @@ window.onload = function () {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  spoilersActive(menuArrows, sublists);
+  function ibg() {
+    $.each($(".ibg"), function (index, val) {
+      if ($(this).find("img").length > 0) {
+        $(this).css(
+          "background-image",
+          'url("' + $(this).find("img").attr("src") + '")'
+        );
+      }
+    });
+  }
+
   ibg();
-  isWebp();
-  checkDevice(menuArrows);
+
+  $(".menu__arrow").click(function () {
+    $(this).toggleClass("_active");
+    $(this).closest("li").find(".menu__sublist").slideToggle("slow");
+    $(this).closest("li").find(".menu__link").toggleClass("orange");
+  });
+
+  $(".search-input input").click(function () {
+    $(".search-input img").addClass("none");
+  });
 
   // SCROLL
-  if (menuLinksData) {
-    menuLinksData.forEach((menuLink) => {
-      menuLink.addEventListener("click", onMenuLinkClick);
-    });
-    function onMenuLinkClick(e) {
-      const menuLink = e.target;
-      if (
-        menuLink.dataset.goto &&
-        document.querySelector(menuLink.dataset.goto)
-      ) {
-        const gotoBlock = document.querySelector(menuLink.dataset.goto);
-        const gotoBlockValue =
-          gotoBlock.getBoundingClientRect().top +
-          pageYOffset -
-          document.querySelector("header").offsetHeight;
-        window.scrollTo({
-          top: gotoBlockValue,
-          behavior: "smooth",
-        });
-        e.preventDefault();
-      }
-    }
-  }
+  // if (menuLinksData) {
+  //   menuLinksData.forEach((menuLink) => {
+  //     menuLink.addEventListener("click", onMenuLinkClick);
+  //   });
+  //   function onMenuLinkClick(e) {
+  //     const menuLink = e.target;
+  //     if (
+  //       menuLink.dataset.goto &&
+  //       document.querySelector(menuLink.dataset.goto)
+  //     ) {
+  //       const gotoBlock = document.querySelector(menuLink.dataset.goto);
+  //       const gotoBlockValue =
+  //         gotoBlock.getBoundingClientRect().top +
+  //         pageYOffset -
+  //         document.querySelector("header").offsetHeight;
+  //       window.scrollTo({
+  //         top: gotoBlockValue,
+  //         behavior: "smooth",
+  //       });
+  //       e.preventDefault();
+  //     }
+  //   }
+  // }
 
   // BURGER
   if (iconMenu) {
@@ -89,11 +102,30 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // CLOSE MENU WHEN CLICK ON LINK
-  for (const link of menuLinks) {
-    link.onclick = () => {
-      document.body.classList.remove("_lock");
-      iconMenu.classList.remove("_active");
-      menuBody.classList.remove("_active");
-    };
+  // for (const link of menuLinks) {
+  //   link.onclick = () => {
+  //     document.body.classList.remove("_lock");
+  //     iconMenu.classList.remove("_active");
+  //     menuBody.classList.remove("_active");
+  //   };
+  // }
+  // ACTION LISTEN
+  function documentActions(e) {
+    const targetElement = e.target;
+    if (
+      !targetElement.closest(".menu__arrow") &&
+      document.querySelector(".menu__arrow._active") &&
+      !targetElement.closest(".menu__sublist") &&
+      document.querySelector(".menu__sublist.open")
+    ) {
+      for (const arrow of arrows) {
+        arrow.classList.remove("_active");
+      }
+      for (const sublist of sublists) {
+        sublist.classList.remove("open");
+      }
+    }
+    // console.log(targetElement)
   }
+  document.addEventListener("click", documentActions);
 });
